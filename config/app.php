5d6e537c183e580440617a7aa54b4d1c00d3de01
Alok Paul <esanalok@gmail.com>
2019-04-20 02:42:16 +0600

    remove stripe from providers

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Cartalyst\Stripe\Laravel\StripeServiceProvider::class
+
 
 
     ],
@@ -227,7 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Stripe' => Cartalyst\Stripe\Laravel\Facades\Stripe::class,
+
 
 
     ],
