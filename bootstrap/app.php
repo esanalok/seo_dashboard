c8d408bbb3fae7573924fa301dc56bdf857c3566
Alok Paul <esanalok@gmail.com>
2019-04-21 02:47:45 +0600

    slug change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,8 @@ $app = new Illuminate\Foundation\Application(
 |
 */
 
+
+
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
     App\Http\Kernel::class
@@ -51,5 +53,7 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
+
+
 
 return $app;
