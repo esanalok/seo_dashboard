63c49c8b2fdb0d141ad25e149a7dfa8426039a4c
Alok Paul <esanalok@gmail.com>
2019-04-21 02:50:51 +0600

    slug change

--- config/database.php
+++ config/database.php
@@ -44,9 +44,9 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => 'qfwtzxvdud',
+            'username' => 'qfwtzxvdud',
+            'password' => 'upeJpYPE8e',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -129,3 +129,8 @@ return [
     ],
 
 ];
+
+
+// 'database' => 'qfwtzxvdud',
+// 'username' => 'qfwtzxvdud',
+// 'password' => 'upeJpYPE8e',
